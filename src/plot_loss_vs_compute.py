13928f8d88fbc3b5c4eef4b5d6d2d39df27e3c2b
David Moser <david.moser@gmx.net>
2025-04-29 19:32:26 +0200

    simple linear estimate

--- src/plot_loss_vs_compute.py
+++ src/plot_loss_vs_compute.py
@@ -21,6 +21,11 @@ files = [
 
 plt.figure()
 
+min_loss = 1e20
+max_loss = 0
+min_compute = 1e20
+max_compute = 0
+
 for file in files:
     with open(file, 'r') as f:
         data = json.load(f)
@@ -31,7 +36,12 @@ for file in files:
     _, eval_loss = zip(*results.eval_loss)
 
     steps = np.array(steps)
-    compute = results.config.flops_per_step() * steps / (1e15 * 24 * 60 *60) if pf_days else 1
+    compute = results.config.flops_per_step() * steps / (1e15 * 24 * 60 *60 if pf_days else 1)
+
+    min_compute = min(min_compute, compute[10])
+    max_compute = max(max_compute, compute[-1])
+    min_loss = min(min_loss, eval_loss[-1])
+    max_loss = max(max_loss, eval_loss[10])
 
     plt.loglog(compute[10:], eval_loss[10:], label=f"{results.config.num_parameters(False)/1e6:.1f}m", linewidth=1)
 
@@ -41,3 +51,7 @@ plt.legend()
 plt.grid(True, which="both", linestyle="--", linewidth=0.5, alpha=0.5)
 plt.tight_layout()
 plt.show()
+
+slope = (np.log(min_loss) - np.log(max_loss)) / (np.log(max_compute) - np.log(min_compute))
+intercept = np.log(min_loss) - slope * np.log(max_compute)
+print(f"slope: {slope:.4f}, intercept: {intercept:.4f}, C_0: {np.exp(-intercept/slope):.4f}")
